b73f2a454c84b664334a91b5158a724c6c3cfbe1
Michaelcs71 <michaelcaesario71@gmail.com>
2024-06-14 03:18:28 -0500

    submit

--- app/src/main/java/com/example/myapplication/FormPertama.java
+++ app/src/main/java/com/example/myapplication/FormPertama.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -36,6 +38,16 @@ public class FormPertama extends AppCompatActivity {
 
             }
         });
+
+        Button continueButton = findViewById(R.id.continue_button_pertama);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormPertama.this, terimakasih.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
